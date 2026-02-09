a18b05a22b4cc28c18e4a937dd5f5d180df90a4c
Giftmuli <giftmuli6742@mail.com>
2026-02-09 13:52:36 +0300

    index.php change

---
+++ links.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="index.php">Home</a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="products.php">Products</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="sale.php">Sale</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
---
+++ sale.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bootstrap-5.3.7-dist/js/bootstrap.bundle.min.js" rel="stylesheet">
+    <link href="bootstrap-5.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Point of sale</title>
+</head>
+<body>
+<?php include 'links.php'; ?>
+
+<div class="container">
+Sale
+</div>
+    
+
+</body>
+</html>
