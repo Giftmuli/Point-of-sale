d3171e8c32b4b80d4645da356bd9403b59e0cda8
Giftmuli <giftmuli6742@mail.com>
2026-02-16 15:12:09 +0300

    add restrictions

--- links.php
+++ links.php
@@ -2,18 +2,30 @@
 <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
   <div class="container-fluid">
     <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active" href="index.php">Home</a>
-      </li>
-     <li class="nav-item">
-        <a class="nav-link" href="products.php">Products</a>
-      </li>
-      <li class="nav-item">
+
+    <?php
+    if(isset($_SESSION['username'])){
+        echo '<li class="nav-item">
+                <a class="nav-link active" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
         <a class="nav-link" href="sale.php">Sale</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="user.php">Users</a>
-      </li>
+      </li>';
+    }
+    ?>
+     
+     <?php
+
+     if (isset($_SESSION['username']) && $_SESSION['role'] == 'Admin') {
+
+         echo '<li class="nav-item">
+                <a class="nav-link" href="products.php">Products</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="user.php">Users</a>
+              </li>';
+     }
+      ?>
 
       <?php
       if (isset($_SESSION['username'])) {
